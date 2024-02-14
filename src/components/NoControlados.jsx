7ee117352750cc713252adef10d865d6eac77a85
Benji-Mtz <benjim.flores@outlook.com>
2024-02-14 11:56:19 -0600

    adding form no controled

--- src/components/NoControlados.jsx
+++ src/components/NoControlados.jsx
@@ -7,7 +7,20 @@ const NoControlado = () => {
     // para form-data necesitamos name en los campos
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Click");
+        console.log(form.current);
+
+        // Capturar los datos
+        const data = new FormData(form.current);
+        console.log([...data.entries()]);
+
+        // El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.
+        const { title, description, state } = Object.fromEntries([...data.entries()]);
+        
+        // validar los datos
+        if (!title.trim()) return console.log("llena el campo title");
+        
+        // Enviar los datos 
+        console.log(title, description, state);
     }
 
 
@@ -19,13 +32,15 @@ const NoControlado = () => {
                 placeholder="Ingrese Todo" 
                 className="form-control mb-2" 
                 name="title"
+                defaultValue={"todo #1"}
             />
             <textarea  
                 className="form-control mb-2" 
                 placeholder="Ingrese Descripción" 
                 name="description"
+                defaultValue={"descripcion #1"}
             />
-            <select className="form-select mb-2" name="state">
+            <select className="form-select mb-2" name="state" defaultValue={"completado"}>
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Completado</option>
             </select>
